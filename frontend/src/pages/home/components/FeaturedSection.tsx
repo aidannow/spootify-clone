c833fcb4faf0233ea203233b0cd7d50fea8953c5
Aidan <u7926126@anu.edu.au>
2026-02-05 19:25:36 +0800

    Creating the main home page layout.
    Constructed the Featured, Made For You, and Trending sections.
    
    Utilised existing song routes to get random songs for each section.
    However, this may be changed in the future to use specific curated songs, as well as implementing Spotify API for better song selection.

---
+++ frontend/src/pages/home/components/FeaturedSection.tsx
@@ -0,0 +1,37 @@
+import FeaturedSectionSkeleton from "@/components/skeletons/FeaturedSectionSkeleton";
+import { useMusicStore } from "@/stores/useMusicStore";
+
+const FeaturedSection = () => {
+  const {isLoading, featuredSongs, error } = useMusicStore();
+
+  if(isLoading) return <FeaturedSectionSkeleton />
+
+  if(error) return <p className="text-red-500 mb-4 text-lg">{error}</p>
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
+
+    { featuredSongs.map((song) => (
+      <div key={song._id}
+        className="flex itmes-center bg-zinc-800/50 rounded-md overflow-hidden
+        hover:bg-zinc-700/50 transition-colors cursor-pointer relative"
+      >
+        {/* Song Image */}
+        <img src={song.imageUrl} alt={song.title} className="w-16 sm:w-20
+        h-16 sm:h-20 object-cover shrink-0"
+        />
+        {/* Title and Artist Name */}
+        <div className="flex-1 p-4">
+          <p className="font-medium truncate">{song.title}</p>
+          <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
+        </div>
+      </div>
+      // Add play button
+    ))}
+
+
+    </div>
+  )
+}
+
+export default FeaturedSection
--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -8,9 +8,15 @@ interface MusicStore {
   isLoading: boolean;
   error: string | null;
   currentAlbum: Album | null;
+  madeForYouSongs: Song[];
+  trendingSongs: Song[];
+  featuredSongs: Song[];
 
   fetchAlbums: () => Promise<void>; // Because its async
   fetchAlbumById: (id: string) => Promise<void>;
+  fetchFeaturedSongs: () => Promise<void>;
+  fetchTrendingSongs: () => Promise<void>;
+  fetchMadeForYouSongs: () => Promise<void>;
 }
 
 export const useMusicStore = create<MusicStore>((set) => ({
@@ -19,6 +25,9 @@ export const useMusicStore = create<MusicStore>((set) => ({
   isLoading: false,
   error: null,
   currentAlbum: null,
+  madeForYouSongs: [],
+  trendingSongs: [],
+  featuredSongs: [],
 
   fetchAlbums: async () => {
     set({
@@ -44,6 +53,42 @@ export const useMusicStore = create<MusicStore>((set) => ({
       set({ currentAlbum: response.data });
     } catch (error) {
       set({ error: 'Failed to fetch album by ID' });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+
+  fetchFeaturedSongs: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axiosInstance.get('/songs/featured');
+      set({ featuredSongs: response.data });
+    } catch (error:any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+
+  fetchTrendingSongs: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axiosInstance.get('/songs/trending');
+      set({ trendingSongs: response.data });
+    } catch (error:any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+  
+  fetchMadeForYouSongs: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axiosInstance.get('/songs/made-for-you');
+      set({ madeForYouSongs: response.data });
+    } catch (error:any) {
+      set({ error: error.response.data.message });
     } finally {
       set({ isLoading: false });
     }
