100e7f368416cf2bb2552d968a90f07baf9c2a0e
Aidan <u7926126@anu.edu.au>
2026-02-15 23:57:17 +0800

    Create actual functionality of detecting Mobile Screen and adjusting the layout accordingly.

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -10,9 +10,22 @@ import LeftSideBar from "./components/LeftSideBar";
 import FriendsActivity from "./components/FriendsActivity";
 import AudioPlayer from "./components/AudioPlayer";
 import PlaybackControls from "./components/PlaybackControls";
+import { useEffect, useState } from "react";
 
 const MainLayout = () => {
-  const isMobile = false;
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    }
+
+    checkMobile();
+    window.addEventListener('resize', checkMobile); // Whenever user tries to resize window then check mobile
+    return () => window.removeEventListener('resize', checkMobile);
+  },[])
+
   return (
     <div className="h-screen bg-black text-white flex flex-col">
       <ResizablePanelGroup
@@ -38,21 +51,26 @@ const MainLayout = () => {
           <Outlet />
         </ResizablePanel>
 
-        <ResizableHandle className="w-2 bg-black rounded-lg tracking-colors" />
+        {/* Show friends activity only if not mobile screen */}
+        {!isMobile && (
+          <>
+            <ResizableHandle className="w-2 bg-black rounded-lg tracking-colors" />
 
-        {/* Right sidebar */}
-        <ResizablePanel
-          defaultSize={20}
-          minSize={0}
-          maxSize={25}
-          collapsedSize={0}
-        >
-          <FriendsActivity/>
-        </ResizablePanel>
+            {/* Right sidebar */}
+            <ResizablePanel
+              defaultSize={20}
+              minSize={0}
+              maxSize={25}
+              collapsedSize={0}
+            >
+              <FriendsActivity />
+            </ResizablePanel>
+          </>
+        )}
+        
       </ResizablePanelGroup>
 
       <PlaybackControls />
-
     </div>
   );
 };
